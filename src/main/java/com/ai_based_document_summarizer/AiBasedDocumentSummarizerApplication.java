60ed617848319f6dfb68a7635746818bf82201a0
Jayanta Karmakar <jayantakarmakar998@gmail.com>
2025-04-01 12:12:03 +0530

    initialize AI-Based Document Summarizer project with Maven setup and basic application structure

---
+++ src/main/java/com/ai_based_document_summarizer/AiBasedDocumentSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.ai_based_document_summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBasedDocumentSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBasedDocumentSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai_based_document_summarizer/AiBasedDocumentSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_based_document_summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBasedDocumentSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
